f888f9c2bef6203dfcd86002f91c4ab4a3fe0ef8
ff3111755-maker <ff3111755@gmail.com>
2026-02-15 15:35:13 +0530

    Initialize Discord client with command collections

---
+++ client.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+// Collections to store commands
+client.prefixCommands = new Collection();
+client.slashCommands = new Collection();
+
+module.exports = client;
